dcdade524e4e7f6db01f5c14d608503f48f4a74a
Evandro Florscuk <evandro_ff@me.com>
2021-04-12 23:35:17 -0300

    Criação das funções getSelect, getFilters e getFormatedValue dentro do arquivo Model.php

--- public/index.php
+++ public/index.php
@@ -4,5 +4,8 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' =>  'lucas@cod3r.com.br']);
-$user->email = 'lucas@outro.com';
-print_r($user);
+
+
+echo User::getSelect(['id' => 1], 'name, email');
+echo '<br>';
+echo User::getSelect(['name' => 'Chaves']);
