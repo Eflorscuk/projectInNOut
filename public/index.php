d62add15cbebc96538689755307b4a396becba93
Evandro Florscuk <evandro_ff@me.com>
2021-04-12 23:16:08 -0300

    criação dos métodos mágicos dentro de Model.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
+$user = new User(['name' => 'Lucas', 'email' =>  'lucas@cod3r.com.br']);
+$user->email = 'lucas@outro.com';
 print_r($user);
-echo '<br>Fim';
