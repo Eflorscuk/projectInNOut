f531265ec96abec838739b6c1afa04fe1fbd687b
Evandro Florscuk <evandro_ff@me.com>
2021-04-15 12:22:10 -0300

    Resolvido problema de direcionamento, faltam fazer as correções dos warnings do PHP. Muito obrigado pela ajuda: Fernando Wagner e Leonardo Scussel pela ajuda no fórum da Cod3r

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
-require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+ 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
-
+ 
 if($uri === '/' || $uri === '' ||  $uri === '/index.php') {
     $uri = '/login.php';
 }
-
+ 
 require_once(CONTROLLER_PATH . "/{$uri}");
-//require_once("/home/evandro/projectInNOut/src/controllers/day_records.php");
