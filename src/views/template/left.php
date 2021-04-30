d8f6937a16ce5ff019636590b4869c328dc79398
Evandro Florscuk <evandro_ff@me.com>
2021-04-30 16:26:52 -0300

    correção do relógio de Horas trabalhadas - Widget

--- src/views/template/left.php
+++ src/views/template/left.php
@@ -4,47 +4,51 @@
             <li class="nav-item">
                 <a href="day_records.php">
                     <i class="icofont-ui-check mr-2"></i>
-                    Registra Ponto
+                    Registrar Ponto
                 </a>
             </li>
             <li class="nav-item">
-                <a href="day_records.php">
+                <a href="monthly_report.php">
                     <i class="icofont-ui-calendar mr-2"></i>
                     Relatório Mensal
                 </a>
             </li>
+            <?php if($user->is_admin): ?>
             <li class="nav-item">
-                <a href=".php">
+                <a href="manager_report.php">
                     <i class="icofont-chart-histogram mr-2"></i>
                     Relatório Gerencial
                 </a>
             </li>
             <li class="nav-item">
-                <a href=".php">
+                <a href="users.php">
                     <i class="icofont-users mr-2"></i>
                     Usuários
                 </a>
             </li>
+            <?php endif ?>
         </ul>
     </nav>
     <div class="sidebar-widgets">
         <div class="sidebar-widget">
             <i class="icon icofont-hour-glass text-primary"></i>
             <div class="info">
-                <span class="main text-primary" <?= $activeClock === 'workedInterval' ? 'active-clock' : '' ?>>
-                    <?= $workedInterval?>
+                <span class="main text-primary"
+                    <?= $activeClock === 'workedInterval' ? 'active-clock' : '' ?>>
+                    <?= $workedInterval ?>
                 </span>
-                <span class="label text-mute">Horas Trabalhadas</span>
+                <span class="label text-muted">Horas Trabalhadas</span>
             </div>
         </div>
         <div class="division my-3"></div>
         <div class="sidebar-widget">
             <i class="icon icofont-ui-alarm text-danger"></i>
             <div class="info">
-                <span class="main text-danger" <?= $activeClock == 'exitTime' ? 'active-clock' : '' ?>>
-                    <?= $exitTime?>
+                <span class="main text-danger"
+                    <?= $activeClock === 'exitTime' ? 'active-clock' : '' ?>>
+                    <?= $exitTime ?>
                 </span>
-                <span class="label text-mute">Hora de Saída</span>
+                <span class="label text-muted">Hora de Saída</span>
             </div>
         </div>
     </div>
