9c5046e944ed5e78f68032eafa82ede2b8cac46c
Evandro Florscuk <evandro_ff@me.com>
2021-05-03 16:08:49 -0300

    Continuação do controller para o relatório mensal.

--- src/views/monthly_report.php
+++ src/views/monthly_report.php
@@ -1,2 +1,26 @@
-<?php
-print_r($registries);
+<main class="content">
+    <?php
+        renderTitle(
+            'Relatório Mensal',
+            'Acompanhe seu saldo de horas',
+            'icofont-ui-calendar'
+        );
+    ?>
+    <div>
+        <table>
+            <thead>
+                <th>Dia</th>
+                <th>Entrada 1</th>
+                <th>Saída 1</th>
+                <th>Entrada 2</th>
+                <th>Saída 2</th>
+                <th>Saldo</th>
+            </thead>
+            <tbody>
+                <?php
+                    
+                ?>
+            </tbody>
+        </table>
+    </div>
+</main>
